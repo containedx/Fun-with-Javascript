463cea0ef768729b1028a5804df83e64159beb49
Kinga <k.zawarty@intermarum.pl>
2023-04-26 12:59:17 +0200

    started long ago uncommited and not finished memory game

---
+++ memory game/app.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+//array with cards images
+var cards = [
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'blank',
+        img: 'images/blank.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'vegehotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'white',
+        img: 'images/white.png'
+    }
+];
+
+// div class grid
+var grid = document.querySelector('.grid');
+    
+    function createBoard(){
+        
+        for (var i = 0; i < cards.length; i++)
+            {
+                var card = document.createElement('img');
+                card.src = cards[i].img;
+                card.width = 100;
+                card.height = 100;
+                grid.appendChild(card);
+            }
+        
+    }
+
+createBoard();
+    
+})
