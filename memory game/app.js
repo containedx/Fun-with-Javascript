dc0b540066118dca901c14b337c0a217667ad764
Kinga <k.zawarty@intermarum.pl>
2023-04-27 18:51:49 +0200

    flipping cards and some improvements

--- memory game/app.js
+++ memory game/app.js
@@ -7,10 +7,6 @@ var cards = [
         img: 'images/fries.png'
     },
     {
-        name: 'blank',
-        img: 'images/blank.png'
-    },
-    {
         name: 'cheeseburger',
         img: 'images/cheeseburger.png'
     },
@@ -29,26 +25,33 @@ var cards = [
     {
         name: 'pizza',
         img: 'images/pizza.png'
-    },
-    {
-        name: 'white',
-        img: 'images/white.png'
     }
 ];
 
 // div class grid
 var grid = document.querySelector('.grid');
+let cardsChosen = []
+let cardsChosenId = []
     
 function createBoard(){  
     for (var i = 0; i < cards.length; i++)
     {
         var card = document.createElement('img');
         card.setAttribute('src', 'images/blank.png');
-        card.setAttribute('data-id', i)
+        card.setAttribute('id', i)
+        card.addEventListener('click', flipCard)
         grid.appendChild(card);
     }      
 }
 
+    
+function flipCard() {
+    let cardId = this.getAttribute('id')
+    this.setAttribute('src', cards[cardId].img)
+}
+
+    
+    
 createBoard();
     
 })
